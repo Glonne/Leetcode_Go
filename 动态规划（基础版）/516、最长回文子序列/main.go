1dccd3a638572cc8b6985b6aad81ca2f83dcbcae
Glonne <670761798@qq.com>
2024-10-23 16:53:21 +0800

    2024/10/23

---
+++ 动态规划（基础版）/516、最长回文子序列/main.go
@@ -0,0 +1,34 @@
+// 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+
+// 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+package main
+
+import "fmt"
+
+func main() {
+	s := "bbbab"
+	result := longestPalindromeSubseq(s)
+	fmt.Print(result)
+}
+
+// dp[i][j]代表i-j是回文的
+func longestPalindromeSubseq(s string) int {
+	dp := make([][]int, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]int, len(s))
+		dp[i][i] = 1
+	}
+
+	for j := 1; j < len(s); j++ { // 从第二列开始
+		for i := j - 1; i >= 0; i-- {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	fmt.Print(dp)
+	return dp[0][len(s)-1]
+}
